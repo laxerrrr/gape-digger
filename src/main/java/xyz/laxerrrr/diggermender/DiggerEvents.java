7f602e402c2f959e0b3395a9f2d6f59e52142e6b
laxerrrr <laxermario@gmail.com>
2021-09-04 15:04:07 -0300

    fixed events

--- src/main/java/xyz/laxerrrr/diggermender/DiggerEvents.java
+++ src/main/java/xyz/laxerrrr/diggermender/DiggerEvents.java
@@ -9,6 +9,7 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Updates;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -41,52 +42,59 @@ public class DiggerEvents {
     public static int currentPartition;
     public static boolean areacleared = false;
     public static long TICKS = 0;
+    public static boolean inGame = false;
     int playerID;
     UUID playerUUID;
     File file = new File(Minecraft.getMinecraft().gameDir.toString() + "/digger-mender");
 
 
-    // @SubscribeEvent
-   // public void playerJoinWorldEvent(EntityJoinWorldEvent event) {
-        //Minecraft.getMinecraft().player.sendChatMessage("joined");
-	/*
-
-	*/
-   // }
-
-
     @SubscribeEvent
-    public void tickEvent(TickEvent.ClientTickEvent event) throws FileNotFoundException { //TODO: ON LOGOUT CANCEL
-        if (event.phase == TickEvent.Phase.END) {
-            TICKS++;
-            if (TICKS % 40 == 0) {
-                    if (!BaritoneAPI.getProvider().getPrimaryBaritone().getBuilderProcess().isActive() && !BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().isActive() && Digger.digging) {
-                        System.out.println("bapi && bapi && digging");
-                        if (!connected) {
-                            System.out.println("Connecting");
-                            Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§4Connecting!"));
-                            Scanner databaseIP = new Scanner(new File(Minecraft.getMinecraft().gameDir.toString() + "/Digger/databaseIP"));
-                            UUID playerUUID = Minecraft.getMinecraft().player.getUniqueID();
-                            System.out.println("start of connection");
-                            String ip = databaseIP.next();
-                            MongoClientURI uri = new MongoClientURI("mongodb://" + databaseIP.next() + ":" + databaseIP.next() + "@" + ip + "/?authSource=admin");
-                            mongoClient = new MongoClient(uri);
-                            database = mongoClient.getDatabase("digging-operation");
-                            partitionsDatabase = mongoClient.getDatabase("partitions");
-                            partitionsCollection = partitionsDatabase.getCollection("partitions");
-                            collection = database.getCollection("bots-progress");
-                            partitions = partitionsCollection.find().first();
-                            try {
-                                s = new Scanner(new File(Minecraft.getMinecraft().gameDir + "/Digger/partitions"));
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            }
-                            connected = true;
-                            Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§aConnected!"));
+    public void playerJoinEvent(PlayerEvent.PlayerLoggedInEvent event) {
+        System.out.println("Logged in");
+        inGame = true;
+   }
+
+   public void playerLoggedOutEvent(PlayerEvent.PlayerLoggedOutEvent event){
+        inGame = false;
+        BaritoneAPI.getProvider().getPrimaryBaritone().getCommandManager().execute("cancel");
+        mongoClient.close();
+   }
+
+
+    @SubscribeEvent //TODO: Fix whitespace
+    public void tickEvent(TickEvent.ServerTickEvent event) throws FileNotFoundException { //Fires every start and end of Client Tick
+        if (event.phase == TickEvent.Phase.END) { //Fires at end of tick
+            TICKS++; //Counts ticks
+            if (TICKS % 40 == 0) { //If ticks are divisible by 40, then go on
+                if (inGame) {
+                    if (!connected) { //Initial connection logic
+                        System.out.println("Connecting");
+                        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§4Connecting!"));
+                        Scanner databaseIP = new Scanner(new File(Minecraft.getMinecraft().gameDir.toString() + "/Digger/databaseIP"));
+                        UUID playerUUID = Minecraft.getMinecraft().player.getUniqueID();
+                        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString(playerUUID.toString()));
+                        System.out.println("start of connection");
+                        String ip = databaseIP.next();
+                        MongoClientURI uri = new MongoClientURI("mongodb://" + databaseIP.next() + ":" + databaseIP.next() + "@" + ip + "/?authSource=admin");
+                        mongoClient = new MongoClient(uri);
+                        database = mongoClient.getDatabase("digging-operation");
+                        partitionsDatabase = mongoClient.getDatabase("partitions");
+                        partitionsCollection = partitionsDatabase.getCollection("partitions");
+                        collection = database.getCollection("bots-progress");
+                        partitions = partitionsCollection.find().first(); //Partitions list
+                        doc = collection.find(eq("UUID", playerUUID)).first();
+                        try {
+                            s = new Scanner(new File(Minecraft.getMinecraft().gameDir + "/Digger/partitions"));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
                         }
+                        connected = true;
+                        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§aConnected!"));
+                    }
+                    if (!BaritoneAPI.getProvider().getPrimaryBaritone().getBuilderProcess().isActive() && !BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().isActive() && Digger.digging) { //Checks if Baritone is not busy and if the bot has to be active
                         switch (stage) {
-                            case 0: //ONLY FOR INITIAL MONGO LOGIC
-                                if (doc == null) {
+                            case 0: //Only fired on first run through
+                                if (doc == null) { //If mongo can't find the bot's progress document in the database, then create one
                                     ArrayList<Integer> tempparts = new ArrayList<Integer>();
                                     tempparts.add(s.nextInt());
                                     tempparts.add(s.nextInt());
@@ -95,21 +103,20 @@ public class DiggerEvents {
                                             .append("currentlayer", 72);
                                     tempdoc.append("allocated", tempparts);
                                     collection.insertOne(tempdoc);
+                                    doc = tempdoc;
                                 }
                                 stage = 1;
                                 break;
                             case 1: //Set correct coordinates and clear area
-                                doc = collection.find(eq("UUID", playerUUID)).first();
                                 allocatedRange = (ArrayList<Integer>) doc.get("allocated");
                                 currentPartition = collection.find(eq("UUID", playerUUID)).first().getInteger("currentpartition");
                                 currentLayer = collection.find(eq("UUID", playerUUID)).first().getInteger("currentlayer"); //Get current layer from database TODO: only check when new layer
                                 ArrayList<Integer> xzcorner = new ArrayList<Integer>(); //Get the X and Z corner block coordinates for the partition
-                                xzcorner.clear();
                                 xzcorner = (ArrayList<Integer>) partitions.get(Integer.toString(currentPartition));
                                 if (currentPartition > allocatedRange.get(1)) {
-                                    Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("wat"));
+                                    Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("wat")); //TODO: Add logic here for when bot goes over allocated range of partitions
                                 }
-                                if (!areacleared) {
+                                if (!areacleared) { //Clears top of partition before row-digging logic
                                     Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§bClearing top!"));
                                     coord = new BetterBlockPos(xzcorner.get(0), 72, xzcorner.get(1)); //Set the starting row coordinate
                                     Digger.clearTop(coord);
@@ -126,7 +133,7 @@ public class DiggerEvents {
                                 rowAmount++;
                                 stage = 4;
                                 break;
-                            case 4: //TODO: remove extra cases
+                            case 4: // (Just room for more logic) TODO: remove extra cases
                                 stage = 5;
                                 break;
                             case 5:
@@ -143,19 +150,18 @@ public class DiggerEvents {
                                     Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("§bDone layer"));
                                     rowAmount = 0;
                                     currentLayer -= 2;
-                                    if (currentLayer <= 30) {
+                                    if (currentLayer <= 30) { //Update database when all layers of partition are done
                                         collection.updateOne(eq("UUID", playerUUID), Updates.set("currentpartition", (currentPartition + 1)));
                                         currentLayer = 72;
                                         areacleared = false;
                                     }
-                                    collection.updateOne(eq("UUID", playerUUID), Updates.set("currentlayer", currentLayer));
+                                    collection.updateOne(eq("UUID", playerUUID), Updates.set("currentlayer", currentLayer)); //Always update layer #
                                 }
                                 stage = 1;
                                 break;
-                            case 9:
-                                stage = 1; //TEMP
                         }
                     }
+                }
                 }
             }
         }
