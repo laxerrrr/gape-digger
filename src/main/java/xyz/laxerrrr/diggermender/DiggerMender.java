74655474b9e9ca0940a96c1eb88cd02b8f0fb36a
laxerrrr <laxermario@hotmail.ca>
2021-10-24 16:16:26 -0300

    schizoid modification

--- src/main/java/xyz/laxerrrr/diggermender/DiggerMender.java
+++ src/main/java/xyz/laxerrrr/diggermender/DiggerMender.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 import net.minecraftforge.client.ClientCommandHandler;
 
 import java.io.File;
@@ -23,7 +22,6 @@ public class DiggerMender
     public static final String NAME = "Digger Mender";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
